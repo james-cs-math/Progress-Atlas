cb09cc28728ddada6dae24e094b76ef8fc5a29d5
James Ruz <jamesruz@192.168.1.12>
2026-04-04 15:43:57 +0800

    fix base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This tells Vite your site is hosted at /Progress-Atlas/
+  base: '/Progress-Atlas/', 
+
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
@@ -17,6 +19,6 @@ export default defineConfig({
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // File types to support raw imports.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
