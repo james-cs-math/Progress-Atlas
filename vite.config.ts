5d9ab504ca719db03ff810929168b8ccb0ff4394
James Ruz <jamesruz@192.168.1.12>
2026-04-09 16:33:56 +0800

    ci: force .env creation and fix base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,17 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This tells Vite your site is hosted at /Progress-Atlas/
+  // ADD THIS LINE: replace 'Progress-Atlas' with your actual repository name
   base: '/Progress-Atlas/', 
-
   plugins: [
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
